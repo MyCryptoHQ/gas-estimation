46d780f4da0661c677ea5b4d90c6eb78693a6184
Frederik Bolding <frederik.bolding@gmail.com>
2021-09-15 12:00:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# MyCrypto EIP 1559 Gas Estimation
+
+**WORK IN PROGRESS**
+
+The MyCrypto EIP 1559 gas estimation strategy, now provided as a library.
---
+++ jest.config.ts
@@ -0,0 +1,4 @@
+export default {
+  testPathIgnorePatterns: ['/node_modules/', '/lib/'],
+  collectCoverageFrom: ['**/src/**/*.{js,jsx,ts,tsx}', '!**/src/types/**']
+};
