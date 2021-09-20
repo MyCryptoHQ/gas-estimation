c2c2a6da8f6d3785ac1a9763fee788dc7eb76232
Frederik Bolding <frederik.bolding@gmail.com>
2021-09-20 16:20:16 +0200

    Test both functions

--- src/eip1559.test.ts
+++ src/eip1559.test.ts
@@ -1,4 +1,4 @@
-import { estimateFees, FALLBACK_ESTIMATE } from './eip1559';
+import { estimateFees, calculateFees, FALLBACK_ESTIMATE } from './eip1559';
 
 const block = {
   hash: '0x38b34c2313e148a0916406a204536c03e5bf77312c558d25d3b63d8a4e30af47',
@@ -30,138 +30,115 @@ describe('estimateFees', () => {
   const mockProvider = {
     send: jest.fn()
   };
-  it('estimates without using priority fees', () => {
-    mockProvider.send.mockResolvedValueOnce(block);
+  it('estimates using calculateFees', async () => {
+    mockProvider.send.mockImplementation((method) => {
+      if (method === 'eth_feeHistory') {
+        return feeHistory;
+      }
+      return { ...block, baseFeePerGas: '0x174876e800' };
+    });
     return expect(estimateFees(mockProvider)).resolves.toStrictEqual({
+      baseFee: 100000000000n,
+      maxFeePerGas: 160000000000n,
+      maxPriorityFeePerGas: 5000000000n
+    });
+  });
+  it('falls back if no baseFeePerGas on block', async () => {
+    mockProvider.send.mockResolvedValueOnce({ ...block, baseFeePerGas: undefined });
+    return expect(estimateFees(mockProvider)).resolves.toStrictEqual(FALLBACK_ESTIMATE);
+  });
+});
+
+describe('calculateFees', () => {
+  it('estimates without using priority fees', () => {
+    const baseFee = BigInt(block.baseFeePerGas);
+    return expect(calculateFees(baseFee, feeHistory)).toStrictEqual({
       baseFee: 10000000000n,
       maxFeePerGas: 20000000000n,
-      maxPriorityFeePerGas: 3000000000n
+      maxPriorityFeePerGas: 5000000000n
     });
   });
 
-  it('estimates priority fees', async () => {
-    mockProvider.send.mockImplementation((method) => {
-      if (method === 'eth_feeHistory') {
-        return feeHistory;
-      }
-      return { ...block, baseFeePerGas: '0x174876e800' };
-    });
-    return expect(estimateFees(mockProvider)).resolves.toStrictEqual({
+  it('estimates priority fees', () => {
+    const baseFee = BigInt('0x174876e800');
+    return expect(calculateFees(baseFee, feeHistory)).toStrictEqual({
       baseFee: 100000000000n,
       maxFeePerGas: 160000000000n,
       maxPriorityFeePerGas: 5000000000n
     });
   });
 
-  it('estimates priority fees removing low outliers', async () => {
-    mockProvider.send.mockImplementation((method) => {
-      if (method === 'eth_feeHistory') {
-        return {
-          ...feeHistory,
-          reward: [
-            ['0x1'],
-            ['0x1'],
-            ['0x1'],
-            ['0x1'],
-            ['0x1'],
-            ['0x1a13b8600'],
-            ['0x12a05f1f9'],
-            ['0x3b9aca00'],
-            ['0x1a13b8600']
-          ]
-        };
-      }
-      return { ...block, baseFeePerGas: '0x174876e800' };
-    });
-    return expect(estimateFees(mockProvider)).resolves.toStrictEqual({
+  it('estimates priority fees removing low outliers', () => {
+    const baseFee = BigInt('0x174876e800');
+    return expect(
+      calculateFees(baseFee, {
+        ...feeHistory,
+        reward: [
+          ['0x1'],
+          ['0x1'],
+          ['0x1'],
+          ['0x1'],
+          ['0x1'],
+          ['0x1a13b8600'],
+          ['0x12a05f1f9'],
+          ['0x3b9aca00'],
+          ['0x1a13b8600']
+        ]
+      })
+    ).toStrictEqual({
       baseFee: 100000000000n,
       maxFeePerGas: 160000000000n,
       maxPriorityFeePerGas: 5000000000n
     });
   });
 
-  it('uses 1.6 multiplier for base if above 40 gwei', async () => {
-    mockProvider.send.mockImplementation((method) => {
-      if (method === 'eth_feeHistory') {
-        return feeHistory;
-      }
-      return { ...block, baseFeePerGas: '0x11766ffa76' };
-    });
-    return expect(estimateFees(mockProvider)).resolves.toStrictEqual({
+  it('uses 1.6 multiplier for base if above 40 gwei', () => {
+    const baseFee = BigInt('0x11766ffa76');
+    return expect(calculateFees(baseFee, feeHistory)).toStrictEqual({
       baseFee: 75001494134n,
       maxFeePerGas: 120000000000n,
-      maxPriorityFeePerGas: 3000000000n
+      maxPriorityFeePerGas: 5000000000n
     });
   });
 
-  it('uses 1.4 multiplier for base if above 100 gwei', async () => {
-    mockProvider.send.mockImplementation((method) => {
-      if (method === 'eth_feeHistory') {
-        return feeHistory;
-      }
-      return { ...block, baseFeePerGas: '0x2e90edd000' };
-    });
-    return expect(estimateFees(mockProvider)).resolves.toStrictEqual({
+  it('uses 1.4 multiplier for base if above 100 gwei', () => {
+    const baseFee = BigInt('0x2e90edd000');
+    return expect(calculateFees(baseFee, feeHistory)).toStrictEqual({
       baseFee: 200000000000n,
       maxFeePerGas: 280000000000n,
       maxPriorityFeePerGas: 5000000000n
     });
   });
 
-  it('uses 1.2 multiplier for base if above 200 gwei', async () => {
-    mockProvider.send.mockImplementation((method) => {
-      if (method === 'eth_feeHistory') {
-        return feeHistory;
-      }
-      return { ...block, baseFeePerGas: '0x45d964b800' };
-    });
-    return expect(estimateFees(mockProvider)).resolves.toStrictEqual({
+  it('uses 1.2 multiplier for base if above 200 gwei', () => {
+    const baseFee = BigInt('0x45d964b800');
+    return expect(calculateFees(baseFee, feeHistory)).toStrictEqual({
       baseFee: 300000000000n,
       maxFeePerGas: 360000000000n,
       maxPriorityFeePerGas: 5000000000n
     });
   });
 
-  it('handles baseFee being smaller than priorityFee', async () => {
-    mockProvider.send.mockImplementation((method) => {
-      if (method === 'eth_feeHistory') {
-        return feeHistory;
-      }
-      return { ...block, baseFeePerGas: '0x7' };
-    });
-    return expect(estimateFees(mockProvider)).resolves.toStrictEqual({
+  it('handles baseFee being smaller than priorityFee', () => {
+    const baseFee = BigInt('0x7');
+    return expect(calculateFees(baseFee, undefined)).toStrictEqual({
       baseFee: 7n,
       maxFeePerGas: 3000000000n,
       maxPriorityFeePerGas: 3000000000n
     });
   });
 
-  it('falls back if no baseFeePerGas on block', async () => {
-    mockProvider.send.mockResolvedValueOnce({ ...block, baseFeePerGas: undefined });
-    return expect(estimateFees(mockProvider)).resolves.toStrictEqual(FALLBACK_ESTIMATE);
-  });
-
-  it('falls back if priority fetching fails', async () => {
-    mockProvider.send.mockImplementation((method) => {
-      if (method === 'eth_feeHistory') {
-        return { ...feeHistory, reward: undefined };
-      }
-      return { ...block, baseFeePerGas: '0x45d964b800' };
-    });
-    return expect(estimateFees(mockProvider)).resolves.toStrictEqual({
+  it('defaults if no fee history available', () => {
+    const baseFee = BigInt('0x45d964b800');
+    return expect(calculateFees(baseFee, undefined)).toStrictEqual({
       baseFee: 300000000000n,
       maxFeePerGas: 360000000000n,
       maxPriorityFeePerGas: FALLBACK_ESTIMATE.maxPriorityFeePerGas
     });
   });
 
-  it('falls back if gas is VERY high', async () => {
-    mockProvider.send.mockImplementation((method) => {
-      if (method === 'eth_feeHistory') {
-        return feeHistory;
-      }
-      return { ...block, baseFeePerGas: '0x91812d7d600' };
-    });
-    return expect(estimateFees(mockProvider)).resolves.toStrictEqual(FALLBACK_ESTIMATE);
+  it('falls back if gas is VERY high', () => {
+    const baseFee = BigInt('0x91812d7d600');
+    return expect(calculateFees(baseFee, feeHistory)).toStrictEqual(FALLBACK_ESTIMATE);
   });
 });
